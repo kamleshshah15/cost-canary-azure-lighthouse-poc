214ef957af977886b2a8773ac7016961a24b1756
Kamlesh Shah <kamlesh.shah@techholding.co>
2025-07-04 18:36:36 +0530

    feat:update policy

--- src/generateDeployLink.ts
+++ src/generateDeployLink.ts
@@ -1,5 +1,5 @@
 // file: src/generateDeployLink.ts
-const templateUrl = 'https://yourdomain.com/arm-template/cost-canary-lighthouse.json'; // replace with your hosted URL
+const templateUrl = 'https://kamleshshah15.github.io/cost-canary-azure-lighthouse-poc/arm-template/cost-canary-lighthouse.json'; // replace with your hosted URL
 const encodedUrl = encodeURIComponent(templateUrl);
 
 const deployLink = `https://portal.azure.com/#create/Microsoft.Template/uri/${encodedUrl}`;
